b8f0f1341395a1e8fdb3c738a8ac59f7fbf1ba18
Sanjay-sp-music <sanjaykumarpoonia760@gmail.com>
2025-05-24 18:50:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ addEventListener('fetch', event => {
 });
 
 // Add your bot token here.
-const BOT_TOKEN = "bot token here";
+const BOT_TOKEN = "7633448500:AAEbREbl878uV_h4YzrsCjxUQ-3K_9LdYfQ";
 
 //Pro people: don't change this else your bot will be fucked.
 const API_BASE_URL = "https://probable-berti-frozenbotspvt-17e82b7b.koyeb.app";
